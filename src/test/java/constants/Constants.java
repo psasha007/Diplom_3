e7141e703ccad7552066a342ef1da8c5feec1e07
psasha007 <psasha007@yandex.ru>
2022-11-29 15:58:31 +0300

    Добавил класс Constants

---
+++ src/test/java/constants/Constants.java
@@ -0,0 +1,19 @@
+package constants;
+
+public class Constants {
+
+    // Константа количество секунд для ожидания
+    public static final long TIME_OUT_IN_SECONDS = 10;
+
+    // Константа Url страницы base
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+
+    // Константа Url страницы register
+    public static final String REGISTRATIONS_URL = "https://stellarburgers.nomoreparties.site/register";
+
+    // User email по умолчанию
+    public static final String DEFAULT_EMAIL = "alex12345@yandex.ru";
+
+    // User password по умолчанию
+    public static final String DEFAULT_PASSWORD = "123456";
+}
